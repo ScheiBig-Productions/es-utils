2e352d5851f55e41e86a59c1459084437a1c923a
Marcin Jeznach <scheissig@gmail.com>
2025-06-01 14:16:15 +0200

    initialize project

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog - es-utils
+
+---
+
+### [2025-06-01] - `major`
+
+New version
+
+### [2025-06-01] - `1.0.0`
+
+Initial version
---
+++ release.mjs
@@ -0,0 +1,48 @@
+import { writeFile, readFile } from "fs/promises"
+import { execSync } from "child_process"
+import packageJson from "./package.json" with { type: "json" }
+
+// --- Get CLI Arguments ---
+const [ , , versionArg, message ] = process.argv
+if (!versionArg || !message) {
+	console.error("Usage: node release.mjs <major/minor/patch/version> '<message>'")
+	process.exit(1)
+}
+
+// --- Determine New Version ---
+let newVersion = execSync(`npm version ${versionArg} --no-git-tag-version`).toString().trim()
+// --- Create Git Tag ---
+execSync(`git commit -am "release ${newVersion}: ${message}"`)
+execSync(`git tag ${newVersion}`)
+execSync("git push && git push --tags")
+
+// --- Update `CHANGELOG.md` ---
+const changelogPath = "CHANGELOG.md"
+const changelogEntry = `
+### [${new Date().toISOString().split("T")[ 0 ]}] - \`${newVersion.replace(/v/, "")}\`
+
+${message}`
+
+let changelogContent = `
+# Changelog
+#### es-utils
+
+Simple journal of changes made to package
+
+---`.trimStart()
+
+try {
+	changelogContent = await readFile(changelogPath, "utf-8")
+} catch (error) {
+	console.warn("⚠️ No existing CHANGELOG.md found, creating a new one.")
+}
+
+const changelog = (() => {
+	let [ start, end ] = changelogContent.split("---", 2)
+	start += "---\n"
+	return { start, end }
+})()
+
+await writeFile(changelogPath, changelog.start + changelogEntry + changelog.end)
+
+console.log(`✅ Released ${newVersion} successfully!`)
